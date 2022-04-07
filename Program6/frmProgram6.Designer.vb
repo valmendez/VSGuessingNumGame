4debea2c4c21e365bd60bd38e99943208985519d
Valeria <valeriam.bonilla07@gmail.com>
2022-04-06 23:32:36 -0400

    Add project files.

---
+++ Program6/frmProgram6.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProgram6
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.lblHint = New System.Windows.Forms.Label()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnEnter.Location = New System.Drawing.Point(127, 92)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(505, 23)
+        Me.btnEnter.TabIndex = 0
+        Me.btnEnter.Text = "ENTER"
+        Me.btnEnter.UseVisualStyleBackColor = False
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.Location = New System.Drawing.Point(127, 134)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(505, 26)
+        Me.txtOutput.TabIndex = 3
+        '
+        'lblHint
+        '
+        Me.lblHint.AutoSize = True
+        Me.lblHint.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHint.Location = New System.Drawing.Point(77, 137)
+        Me.lblHint.Name = "lblHint"
+        Me.lblHint.Size = New System.Drawing.Size(44, 20)
+        Me.lblHint.TabIndex = 4
+        Me.lblHint.Text = "hint:"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.BackColor = System.Drawing.SystemColors.Info
+        Me.lblInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.Location = New System.Drawing.Point(234, 54)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(338, 20)
+        Me.lblInfo.TabIndex = 5
+        Me.lblInfo.Text = "Click ENTER to begin the guessing game"
+        '
+        'frmProgram6
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.lblHint)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Name = "frmProgram6"
+        Me.Text = "Program6"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents lblHint As Label
+    Friend WithEvents lblInfo As Label
+End Class
---
+++ Program6/frmProgram6.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmProgram6
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        'code for generating a random number from 0-100
+        Dim number As Integer = CInt(Int((100 * Rnd()) + 1))
+
+        'variables
+        Dim userNumber As Integer
+        Dim prompt, title As String 'for InputBox
+
+        Do
+            prompt = "Enter a number until you guess the correct one"
+            title = "Number"
+
+            userNumber = CInt(InputBox(prompt, title)) 'ask user for input
+
+            If userNumber < number Then
+                txtOutput.Text = "Try Again. Your input is too low."
+            ElseIf userNumber > number Then
+                txtOutput.Text = "Try Again. Your input is too high."
+
+            End If
+
+        Loop While (userNumber < number) Or (userNumber > number) 'userNumber <> number
+
+        MessageBox.Show("Correct! Your number matches the randomly generated number :) ")
+
+    End Sub
+End Class
